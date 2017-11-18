2f00d41478d0d65b853e7077cd6b4d1ddf11a0d0
Rustam Kuramshin <leo.in.empty@gmail.ru>
2017-11-18 23:43:55 +0300

    Added "fuck off" word.

--- Mailer.py
+++ Mailer.py
@@ -21,6 +21,7 @@ def send_mail(send_from, send_to, subject, text, server, user_name, user_passwd,
 
     msg.attach(MIMEText(text))
 
+    print("fuck off")
 
     with open (files, 'rb') as file:
         part = MIMEApplication(
